f822b00f05cf0a9fda3b2425c8f68879c2a4942e
CJ Buchmann <cj.buchmann@sendgrid.com>
2012-01-06 16:17:57 -0800

    Modified Isendgrid to add new header object, and removed erroneous dependencies from files

--- SendGrid/SendGrid/Transport/ITransport.cs
+++ SendGrid/SendGrid/Transport/ITransport.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SendGrid.Transport
+﻿namespace SendGrid.Transport
 {
     public interface ITransport
     {
