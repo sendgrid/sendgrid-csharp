b26c82607d13f1ae532f138b94d7eb681c25d658
Tyler Bischel <tyler.bischel@sendgrid.com>
2012-01-10 09:31:45 -0800

    new test file

---
+++ SendGrid/Tests/Transport/TestSMTP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tests.Transport
+{
+    [TestFixture]
+    public class TestSMTP
+    {
+        [Test]
+        public void Deliver()
+        {
+
+        }
+    }
+}
