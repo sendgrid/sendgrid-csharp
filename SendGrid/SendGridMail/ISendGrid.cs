4efe204782471993dd60cd7ac72e5414229094b0
Eric Becking <eric.becking@sendgrid.com>
2012-01-12 10:33:28 -0700

    Documentation for ISendgrid and an update to the headers in SendgGrid.cs

--- SendGrid/SendGridMail/ISendGrid.cs
+++ SendGrid/SendGridMail/ISendGrid.cs
@@ -8,12 +8,20 @@ using System.Text;
 
 namespace SendGridMail
 {
+    /// <summary>
+    /// Internal object to represent the way in which email may be sent.
+    /// The library supports sending through either SMTP or REST interfaces.
+    /// </summary>
     public enum TransportType
     {
         SMTP,
         REST
     };
 
+    /// <summary>
+    /// Represents the basic set of functions that will be called by the user
+    /// includes basic message data manipulation and filter settings
+    /// </summary>
     public interface ISendGrid
     {
         #region Properties
@@ -21,6 +29,7 @@ namespace SendGridMail
         MailAddress[] To { get; set; }
         MailAddress[] Cc { get; }
         MailAddress[] Bcc { get; }
+        Attachment[] Attachments { get; set; }
         String Subject { get; set; }
         IHeader Header { get; set; }
         String Html { get; set; }
@@ -33,24 +42,100 @@ namespace SendGridMail
         #endregion
 
         #region Methods for setting data
+        /// <summary>
+        /// Set the To address.  In this case the input is a single string eg. 'you@company.com'
+        /// </summary>
+        /// <param name="address"></param>
         void AddTo(String address);
+        /// <summary>
+        /// Set the To address.  In this case the expected input is a list of addresses as strings
+        /// </summary>
+        /// <param name="addresses"></param>
         void AddTo(IEnumerable<String> addresses);
+        /// <summary>
+        /// Set the To address.  In this case the expected input is a list of addresses with paramaters.
+        /// The first parameter is a string for the email address. 
+        /// The second paramater is a dictionary containing the key, value pairs for a property's name and its value.
+        /// Currently the only value that you can set with the MailAddress data object is the 'DisplayName' field. 
+        /// </summary>
+        /// <param name="addresssInfo"></param>
         void AddTo(IDictionary<String, IDictionary<String, String>> addresssInfo);
 
+        /// <summary>
+        /// Add the CC address.  In this case the expected input is a single email address eg "you@company.com"
+        /// </summary>
+        /// <param name="address"></param>
         void AddCc(String address);
+        /// <summary>
+        /// Set the CC address.  In this case the expected input is a list of addresses as strings
+        /// </summary>
+        /// <param name="addresses"></param>
         void AddCc(IEnumerable<String> addresses);
+        /// <summary>
+        /// Set the CC address.  In this case the expected input is a list of addresses with paramaters.
+        /// The first parameter is a string for the email address.
+        /// The second paramater is a dictionary containing the key, value pairs for a property's name and its value.
+        /// Currently the only value that you can set with the MailAddress data object is the 'DisplayName' field.
+        /// </summary>
+        /// <param name="addresssInfo"></param>
         void AddCc(IDictionary<String, IDictionary<String, String>> addresssInfo);
 
+        /// <summary>
+        /// Set the Bcc address.  Expects a single email as the input eg "you@company.com"
+        /// </summary>
+        /// <param name="address"></param>
         void AddBcc(String address);
+        /// <summary>
+        /// Set the Bcc address.  Expects a list of emails as an array of strings.
+        /// </summary>
+        /// <param name="addresses"></param>
         void AddBcc(IEnumerable<String> addresses);
+        /// <summary>
+        /// Set the Bcc address.  In this case the expected input is a list of addresses with paramaters.
+        /// The first parameter is a string for the email address.
+        /// The second paramater is a dictionary containing the key, value pairs for a property's name and its value.
+        /// Currently the only value that you can set with the MailAddress data object is the 'DisplayName' field.
+        /// </summary>
+        /// <param name="addresssInfo"></param>
         void AddBcc(IDictionary<String, IDictionary<String, String>> addresssInfo);
 
+        /// <summary>
+        /// Sets a list of substitution values for the email.
+        /// the 'tag' parameter is the value in the email that you'll replace eg. '-name-'
+        /// the 'value' parameter is a list of values that will be substituted in for the tag.
+        /// In our example above the list would be something like ['eric', 'tyler', 'cj'].
+        /// The 'value' parameter expects a 1:1 mapping from email addresses to values.  
+        /// If you are left with more email addresses then values to substitute then the substitution tag will be left blank.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <param name="value"></param>
         void AddSubVal(String tag, params String[] value);
 
+        /// <summary>
+        /// Add an attachment to the message.  
+        /// The 'filePath' paramater expects a fully qualified file path as a string
+        /// </summary>
+        /// <param name="filePath"></param>
         void AddAttachment(String filePath);
+        /// <summary>
+        /// Add an attachment to the message
+        /// The 'attachment' paramater expects an argument of the Attachment type.
+        /// </summary>
+        /// <param name="attachment"></param>
         void AddAttachment(Attachment attachment);
+        /// <summary>
+        /// Add an attachment to the message
+        /// The 'attachment' paramater expects an argument of the Attachment type.
+        /// The 'type' parameter expects an argument of the ContentType type.
+        /// </summary>
+        /// <param name="attachment"></param>
+        /// <param name="type"></param>
         void AddAttachment(Stream attachment, ContentType type);
 
+        /// <summary>
+        /// GetRecipients returns a list of all the recepients by retrieving the to, cc, and bcc lists.
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<String> GetRecipients();
         #endregion
 
