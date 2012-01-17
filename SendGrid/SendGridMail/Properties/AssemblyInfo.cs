607d334cf039d63a7e6361077b0cd8646959ce31
Tyler Bischel <tyler.bischel@sendgrid.com>
2012-01-16 16:59:10 -0800

    updated assembly stuff

--- SendGrid/SendGridMail/Properties/AssemblyInfo.cs
+++ SendGrid/SendGridMail/Properties/AssemblyInfo.cs
@@ -5,10 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("SendGridMail")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A client library for interfacing with the SendGrid API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SendGrid")]
 [assembly: AssemblyProduct("SendGridMail")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
