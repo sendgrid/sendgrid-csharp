8e762563b055afb30b162c139ca7778320280ab8
Tyler Bischel <tyler.bischel@sendgrid.com>
2012-01-16 22:25:03 -0800

    removed redundant code and added some unit tests

--- SendGrid/Tests/TestUtils.cs
+++ SendGrid/Tests/TestUtils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using SendGridMail;
 
 namespace Tests
 {
@@ -10,9 +11,28 @@ namespace Tests
     public class TestUtils
     {
         [Test]
-        public void Test()
+        public void TestSerialize()
         {
-            
+            var testcase = "foo";
+            String result = Utils.Serialize(testcase);
+            Assert.AreEqual("\"foo\"", result);
+
+            var testcase2 = 1;
+            result = Utils.Serialize(testcase2);
+            Assert.AreEqual("1", result);
+        }
+
+        [Test]
+        public void TestSerializeDictionary()
+        {
+            var test = new Dictionary<string, string>
+                           {
+                               {"a", "b"}, 
+                               {"c", "d/e"}
+                           };
+            var result = Utils.SerializeDictionary(test);
+            var expected = "{\"a\":\"b\",\"c\":\"d\\/e\"}";
+            Assert.AreEqual(expected, result);
         }
     }
 }
