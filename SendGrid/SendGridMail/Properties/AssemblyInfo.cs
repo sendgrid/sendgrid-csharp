0407cce8a443f1c1cd9c7b16e8625647458977c8
CJ Buchmann <cj.buchmann@sendgrid.com>
2012-01-16 18:18:16 -0800

    Added NuGet Package parameters and moved CodeScales.dll to lib directory for NuGet

--- SendGrid/SendGridMail/Properties/AssemblyInfo.cs
+++ SendGrid/SendGridMail/Properties/AssemblyInfo.cs
@@ -5,7 +5,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("SendGridMail")]
 [assembly: AssemblyDescription("A client library for interfacing with the SendGrid API")]
 [assembly: AssemblyConfiguration("")]
