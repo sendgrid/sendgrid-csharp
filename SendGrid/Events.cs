24f508687b7d44654ff122ee2d34c72101e5651a
Robert McLaws <robert@advancedrei.com>
2012-09-28 03:07:58 -0400

    Fixed project references and added Generics support for Events.
    - Added generic methods for GetEvent and GetEvents, to enable you to use
    objects inheriting from EventData to retrieve info added through the
    AddUniqueIdentifiers method.

--- SendGrid/Events.cs
+++ SendGrid/Events.cs
@@ -15,12 +15,24 @@ namespace SendGrid
         /// <summary>
         /// Gets a single event from an Stream, such as an MVC Controller's Request.InputStream.
         /// </summary>
-        /// <param name="inputStream"></param>
+        /// <param name="inputStream">The Stream to pull the JSON from.</param>
         /// <returns></returns>
         public static EventData GetEvent(Stream inputStream)
         {
             var json = new StreamReader(inputStream).ReadToEnd();
             return GetEvent(json);
+        }
+
+        /// <summary>
+        /// Gets a single event from an Stream, such as an MVC Controller's Request.InputStream.
+        /// </summary>
+        /// <typeparam name="T">A type inheriting from EventData to use to deserialize the data. Useful when including custom Identifiers.</typeparam>
+        /// <param name="inputStream">The Stream to pull the JSON from.</param>
+        /// <returns></returns>
+        public static T GetEvent<T>(Stream inputStream) where T : EventData
+        {
+            var json = new StreamReader(inputStream).ReadToEnd();
+            return GetEvent<T>(json);
         }
 
         /// <summary>
@@ -31,6 +43,17 @@ namespace SendGrid
         public static EventData GetEvent(string json)
         {
             return JsonConvert.DeserializeObject<EventData>(string.Format("[{0}]", json));
+        }
+
+        /// <summary>
+        /// Gets a single Event from a JSON string.
+        /// </summary>
+        /// <typeparam name="T">A type inheriting from EventData to use to deserialize the data. Useful when including custom Identifiers.</typeparam>
+        /// <param name="json">A string containing the JSON event data.</param>
+        /// <returns></returns>
+        public static T GetEvent<T>(string json) where T : EventData
+        {
+            return JsonConvert.DeserializeObject<T>(string.Format("[{0}]", json));
         }
 
         /// <summary>
@@ -45,6 +68,18 @@ namespace SendGrid
         }
 
         /// <summary>
+        /// Gets a list of Batched Events from an Stream, such as an MVC Controller's Request.InputStream.
+        /// </summary>
+        /// <typeparam name="T">A type inheriting from EventData to use to deserialize the data. Useful when including custom Identifiers.</typeparam>
+        /// <param name="inputStream"></param>
+        /// <returns></returns>
+        public static List<T> GetEvents<T>(Stream inputStream) where T : EventData
+        {
+            var json = new StreamReader(inputStream).ReadToEnd();
+            return GetEvents<T>(json);
+        }
+
+        /// <summary>
         /// Gets a list of Batched Events from a JSON string.
         /// </summary>
         /// <param name="json">A string containing the JSON batched event data.</param>
@@ -53,6 +88,18 @@ namespace SendGrid
         {
             json = string.Format("[{0}]", json.Replace("}" + Environment.NewLine + "{", "},{"));
             return JsonConvert.DeserializeObject<List<EventData>>(json);
+        }
+
+        /// <summary>
+        /// Gets a list of Batched Events from a JSON string.
+        /// </summary>
+        /// <typeparam name="T">A type inheriting from EventData to use to deserialize the data. Useful when including custom Identifiers.</typeparam>
+        /// <param name="json">A string containing the JSON batched event data.</param>
+        /// <returns></returns>
+        public static List<T> GetEvents<T>(string json) where T : EventData
+        {
+            json = string.Format("[{0}]", json.Replace("}" + Environment.NewLine + "{", "},{"));
+            return JsonConvert.DeserializeObject<List<T>>(json);
         }
 
     }
