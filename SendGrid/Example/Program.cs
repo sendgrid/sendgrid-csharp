3b5c2b286df6a6beb0f1ddb13d8871f20c3e798a
brandonmwest <brawest@gmail.com>
2013-01-10 13:45:22 -0700

    Update SendGrid/Example/Program.cs

--- SendGrid/Example/Program.cs
+++ SendGrid/Example/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,18 +15,21 @@ namespace Example
         // this code is used for the SMTPAPI examples
         static void Main(string[] args)
         {
-            var username = "brandonmwest";
-            var password = "!!s3ndgr1d";
-            var from = "brandon.west@sendgrid.com";
+            var username = "sgrid_username";
+            var password = "sgrid_password";
+            var from = "bar@domain.com";
             var to = new List<String>
                          {
-                             "@gmail.com",
+                             "foo@domain.com",
+                             "raz@domain.com"
                          };
 
+            //initialize the SMTPAPI example class
+            var smtpapi = new SMTPAPI(username, password, from, to);
             var restapi = new WEBAPI(username, password, from, to);
 
             //use this section to test out our Web and SMTP examples!
-            restapi.SimpleHTMLEmail();
+            smtpapi.SimpleHTMLEmail();
 
             Console.WriteLine("Done!");
             Console.ReadLine();
