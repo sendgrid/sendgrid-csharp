643dd1a1f0d4021d6c3f47db33b938b74862e96f
Brandon West <brawest@gmail.com>
2014-06-14 10:37:51 -0600

    add conditional compile symbol specifying whether or not to use publickeytoken for friend assemblies

--- SendGrid/SendGridMail/Properties/AssemblyInfo.cs
+++ SendGrid/SendGridMail/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿#define build
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -24,13 +26,17 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("193fa200-8430-4206-aacd-2d2bb2dfa6cf")]
+
+#if (build)
+[assembly: InternalsVisibleTo("Tests")]
+#else
 [assembly: InternalsVisibleTo("Tests," + "" +
                               "PublicKey=00240000048000009400000006020000002400005253413100040000010001004126bffd5a4461" +
                               "e915193b2695401cee8d67bb14b252a34e5230e6468582f108aafbe31d39f2059240461d622e86" +
                               "2a294169d5f2659efe0d68b30d7ceee310356c70b54ece3c8c69bbd9db86e07c34ff4fd5d7528b" +
                               "3ddf078d272025cb7a588030c78020f5eb91872b38dc2832f561fe184715bb8edb6f0b3b644de5" +
                               "2bc588ae")]
-
+#endif
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
