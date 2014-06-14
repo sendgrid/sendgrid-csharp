00316ab5775ecdae9632e31ea826bc3419331722
Brandon West <brawest@gmail.com>
2014-06-14 13:59:54 -0600

    remove unnecessary abstraction from test runner

--- SendGrid/.ci/tests.sh
+++ SendGrid/.ci/tests.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -x
 
 mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
-
-runTest(){
-    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
-   if [ $? -ne 0 ]
-   then   
-     exit 1
-   fi
-}
-
+mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror Tests/bin/BuildNet40/Tests.dll
 exit $?
