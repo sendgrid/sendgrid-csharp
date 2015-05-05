fb8b97373b6f92dc3dffa4af8208e08ab2ae579d
MarcelloCarreira <MarcelloCarreira@users.noreply.github.com>
2015-05-04 23:16:04 -0300

    Create SendGridSendAsync.cs
    See Summary description

---
+++ SendGrid/Example/SendGridSendAsync.cs
@@ -0,0 +1,60 @@
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using SendGrid;
+using System.Threading.Tasks;
+using Exceptions;
+
+/// <summary>
+///
+/// Summary description for SendGridSendAsync
+///
+/// This will send the message Async, and will return a string after is completed.
+///
+/// If the delivery is ok, the string will be "OK" (from the HttpStatusCode)
+///
+/// If there is an error, the error will be catch and the string will show the error in this format:
+///
+/// Status Code: [HttpStatusCode]. Errors returned by the API: [error1; error2; etc;]
+///
+/// Example of error:
+/// 
+/// Status Code: BadRequest. Errors returned by the API: Missing subject;
+/// Status Code: BadRequest. Errors returned by the API: invalid API Key;
+///
+/// </summary>
+public class SendGridSendAsync
+{
+    string SendGridApiKey = Convert.ToString(System.Configuration.ConfigurationManager.ConnectionStrings["SendGridApiKey"]);
+
+    public async Task<string> sendMessageAsync(SendGridMessage sendGridMessage)
+    {
+        // Create a Web transport for sending email.
+        var transportWeb = new Web(SendGridApiKey);
+
+        try
+        {
+            string response = await transportWeb.DeliverAsync(sendGridMessage).ConfigureAwait(continueOnCapturedContext: false);
+            return response;
+        }
+        catch (InvalidApiRequestException ex)
+        {
+            string error;
+            string statusCode = "Status Code: " + ex.ResponseStatusCode;
+            string label = ". Errors returned by the API: ";
+            string errors = "";
+
+            for (int i = 0; i <= ex.Errors.Length - 1; i++)
+            {
+                errors = errors + " " + ex.Errors[i] + ";";
+            }
+
+            error = statusCode + label + errors;
+            return error;
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+}
