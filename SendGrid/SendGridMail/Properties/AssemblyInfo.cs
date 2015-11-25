77f9554031c6c85b2bae1989784eb3f687be9abd
Elmer Thomas <elmer@thinkingserious.com>
2015-11-24 19:15:00 -0800

    Preparing for upload to Nuget.org

--- SendGrid/SendGridMail/Properties/AssemblyInfo.cs
+++ SendGrid/SendGridMail/Properties/AssemblyInfo.cs
@@ -58,5 +58,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("6.0.0")]
-[assembly: AssemblyFileVersion("6.0.0")]
+[assembly: AssemblyVersion("6.3.0")]
+[assembly: AssemblyFileVersion("6.3.0")]
