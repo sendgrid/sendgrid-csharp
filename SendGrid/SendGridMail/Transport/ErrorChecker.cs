55529190aff7092b79634b6bf21aa593dcd39284
Maxim Dubrovkin <dubrovkinmaxim@gmail.com>
2016-01-31 21:20:42 +0500

    ErrorChecker was refactored.

--- SendGrid/SendGridMail/Transport/ErrorChecker.cs
+++ SendGrid/SendGridMail/Transport/ErrorChecker.cs
@@ -6,7 +6,6 @@
     using System.Net.Http;
     using System.Threading.Tasks;
     using System.Xml;
-
     using Exceptions;
 
     public static class ErrorChecker
@@ -23,32 +22,32 @@
 
         private static void CheckForErrors(HttpResponseMessage response, Stream stream)
         {
-            if (response.StatusCode != HttpStatusCode.OK)
-            {
-                using (var reader = XmlReader.Create(stream))
-                {
-                    while (reader.Read())
-                    {
-                        if (!reader.IsStartElement())
-                        {
-                            continue;
-                        }
+            if (response.StatusCode == HttpStatusCode.OK) return;
 
-                        switch (reader.Name)
-                        {
-                            case "result":
-                                continue;
-                            case "message":
-                                continue;
-                            case "errors":
-                                reader.ReadToFollowing("error");
-                                var message = reader.ReadElementContentAsString("error", reader.NamespaceURI);
-                                throw new InvalidApiRequestException(response.StatusCode, new[] { message }, response.ReasonPhrase);
-                            case "error":
-                                throw new ProtocolViolationException();
-                            default:
-                                throw new ArgumentException("Unknown element: " + reader.Name);
-                        }
+            using (var reader = XmlReader.Create(stream))
+            {
+                while (reader.Read())
+                {
+                    if (!reader.IsStartElement())
+                        continue;
+
+                    switch (reader.Name)
+                    {
+                        case "result":
+                        case "message":
+                            continue;
+
+                        case "errors":
+                            reader.ReadToFollowing("error");
+                            var message = reader.ReadElementContentAsString("error", reader.NamespaceURI);
+                            throw new InvalidApiRequestException(response.StatusCode, new[] {message},
+                                response.ReasonPhrase);
+
+                        case "error":
+                            throw new ProtocolViolationException();
+
+                        default:
+                            throw new ArgumentException("Unknown element: " + reader.Name);
                     }
                 }
             }
