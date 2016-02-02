b9ffdeaa9d973ffdd9b14837ce2fc7ecf13eb354
Jeremie Desautels <desautelsj@hotmail.com>
2016-02-02 16:52:05 -0600

    Strongly typed GlobalStats

---
+++ SendGrid/SendGrid/Model/Metrics.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+
+namespace SendGrid.Model
+{
+    public class Metrics
+    {
+        [JsonProperty("blocks")]
+        public long Blocks { get; set; }
+
+        [JsonProperty("bounce_drops")]
+        public long BounceDrops { get; set; }
+
+        [JsonProperty("bounces")]
+        public long Bounces { get; set; }
+
+        [JsonProperty("clicks")]
+        public long Clicks { get; set; }
+
+        [JsonProperty("deferred")]
+        public long Deferred { get; set; }
+
+        [JsonProperty("delivered")]
+        public long Delivered { get; set; }
+
+        [JsonProperty("invalid_emails")]
+        public long InvalidEmails { get; set; }
+
+        [JsonProperty("opens")]
+        public long Opens { get; set; }
+
+        [JsonProperty("processed")]
+        public long Processed { get; set; }
+
+        [JsonProperty("requests")]
+        public long Requests { get; set; }
+
+        [JsonProperty("spam_report_drops")]
+        public long SpamReportDrops { get; set; }
+
+        [JsonProperty("spam_reports")]
+        public long SpamReports { get; set; }
+
+        [JsonProperty("unique_clicks")]
+        public long UniqueClicks { get; set; }
+
+        [JsonProperty("unique_opens")]
+        public long UniqueOpens { get; set; }
+
+        [JsonProperty("unsubscribe_drops")]
+        public long UnsubscribeDrops { get; set; }
+
+        [JsonProperty("unsubscribes")]
+        public long Unsubscribes { get; set; }
+    }
+}
---
+++ SendGrid/SendGrid/Utilities/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http;
+
+namespace SendGrid.Utilities
+{
+    public static class Extensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+            if (fieldInfo == null) return value.ToString();
+
+            var attributes = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes == null || attributes.Length == 0) return value.ToString();
+
+            var descriptionAttribute = attributes[0] as DescriptionAttribute;
+            return (descriptionAttribute == null ? value.ToString() : descriptionAttribute.Description);
+        }
+
+        public static void EnsureSuccess(this HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode) return;
+
+            var content = response.Content.ReadAsStringAsync().Result;
+            if (response.Content != null) response.Content.Dispose();
+
+            throw new Exception(content);
+        }
+    }
+}
