431ab71d9f36bc74f6c10ed497a5e759d5985f16
Jeremie Desautels <desautelsj@hotmail.com>
2016-02-15 16:30:23 -0600

    Strongly typed custom fields

---
+++ SendGrid/SendGrid/Model/Field.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SendGrid.Model
+{
+    public class Field
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public FieldType Type { get; set; }
+    }
+}
