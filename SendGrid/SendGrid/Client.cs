db36a089b6dc016c11b3bcd626403b8bbe382157
Elmer Thomas <elmer@thinkingserious.com>
2016-06-09 12:12:02 -0700

    Updated client call to csharp-http-client

--- SendGrid/SendGrid/Client.cs
+++ SendGrid/SendGrid/Client.cs
@@ -30,7 +30,7 @@ namespace SendGrid
             requestHeaders.Add("Authorization", "Bearer " + apiKey);
             requestHeaders.Add("Content-Type", "application/json");
             requestHeaders.Add("User-Agent", "sendgrid/" + Version + " csharp");
-            client = new Client(baseUri, requestHeaders, version);
+            client = new Client(host: baseUri, requestHeaders: requestHeaders, version: version);
         }
     }
 }
