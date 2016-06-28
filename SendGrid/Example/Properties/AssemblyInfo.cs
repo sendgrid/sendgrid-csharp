37936c79ffdfe7702d9589832e89a1b9b1172140
Elmer Thomas <elmer@thinkingserious.com>
2016-06-28 08:54:16 -0700

    Version Bump v7.03: 232 Send mail fails with BadRequest when apostrophe used in sender name

--- SendGrid/Example/Properties/AssemblyInfo.cs
+++ SendGrid/Example/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("7.0.2")]
-[assembly: AssemblyFileVersion("7.0.2")]
+[assembly: AssemblyVersion("7.0.3")]
+[assembly: AssemblyFileVersion("7.0.3")]
