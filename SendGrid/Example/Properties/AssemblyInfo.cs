068447df7fda47e02f89c3011e046d5bb7bfcac4
Elmer Thomas <elmer@thinkingserious.com>
2016-07-18 14:23:39 -0700

    Version Bump v7.0.7: Fix for #256

--- SendGrid/Example/Properties/AssemblyInfo.cs
+++ SendGrid/Example/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("7.0.6")]
-[assembly: AssemblyFileVersion("7.0.6")]
+[assembly: AssemblyVersion("7.0.7")]
+[assembly: AssemblyFileVersion("7.0.7")]
