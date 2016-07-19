26e65a4524cb4787cddedb776d699ade7fcd0c53
Elmer Thomas <elmer@thinkingserious.com>
2016-07-19 13:19:08 -0700

    Version Bump v7.1.0: WebProxy Support

--- SendGrid/Example/Properties/AssemblyInfo.cs
+++ SendGrid/Example/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("7.0.7")]
-[assembly: AssemblyFileVersion("7.0.7")]
+[assembly: AssemblyVersion("7.1.0")]
+[assembly: AssemblyFileVersion("7.1.0")]
