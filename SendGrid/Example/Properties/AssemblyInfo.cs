fa265a1e2638baa645885958431c1750992f9bdc
Elmer Thomas <elmer@thinkingserious.com>
2016-08-24 11:09:39 -0700

    Version Bump v8.0.4: Added TOC to README, Added USE_CASES.md

--- SendGrid/Example/Properties/AssemblyInfo.cs
+++ SendGrid/Example/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("8.0.3")]
-[assembly: AssemblyFileVersion("8.0.3")]
+[assembly: AssemblyVersion("8.0.4")]
+[assembly: AssemblyFileVersion("8.0.4")]
