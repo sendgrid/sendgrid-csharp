2d714b0a12d1449d7051b55d489f8a59c1ecb52c
M3LiNdRu <rogercalaf@gmail.com>
2016-10-18 23:44:32 +0200

    Added Integration Test for SendMailToMultipleRecipients

--- SendGrid/SendGrid.IntegrationTests/MailServiceTest.cs
+++ SendGrid/SendGrid.IntegrationTests/MailServiceTest.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 namespace SendGrid.IntegrationTests
 {
     [TestClass]
-    public class MailServicesTest
+    public class MailServiceTest
     {
         private string _apiKey;
         private SendGrid.SendGridAPIClient _client;
 
-        public MailServicesTest()
+        public MailServiceTest()
         {
             _apiKey = Environment.GetEnvironmentVariable("SENDGRID_APIKEY", EnvironmentVariableTarget.User);
             _client = new SendGridAPIClient(_apiKey, "https://api.sendgrid.com");
@@ -26,9 +26,35 @@ namespace SendGrid.IntegrationTests
             Email to = new Email("test@example.com");
             Content content = new Content("text/plain", "Textual content");
             SendGrid.Models.Mail mail = new SendGrid.Models.Mail(from, subject, to, content);
-            _client.Mail.MailRequest = mail;
+
 
             //Act
+            _client.Mail.MailRequest = mail;
+            var response = await _client.Mail.SendAsync();
+
+            //Assert
+            Assert.IsTrue(response.Success);
+        }
+
+        [TestMethod]
+        public async Task SendMailToMultipleRecipientTest_Success()
+        {
+            //Arrange
+            Email from = new Email("test@example.com");
+            String subject = "Hello World from the SendGrid CSharp Library";
+            Email to = new Email("test@example.com");
+            Content content = new Content("text/plain", "Textual content");
+            SendGrid.Models.Mail mail = new SendGrid.Models.Mail(from, subject, to, content);
+
+            Personalization personalitzation = new Personalization();
+            Email email = new Email();
+            email.Name = "Example User";
+            email.Address = "test1@example.com";
+            personalitzation.AddTo(email);
+            mail.AddPersonalization(personalitzation);
+
+            //Act
+            _client.Mail.MailRequest = mail;
             var response = await _client.Mail.SendAsync();
 
             //Assert
