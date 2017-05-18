cf45fd117f45a9cfd93e5110aaa0f685b434791a
Nate <francois.renaud-philippon.1@ulaval.ca>
2017-05-18 11:20:57 -0400

    Wrong file name in the copyright header

--- src/SendGrid/ISendGridClient.cs
+++ src/SendGrid/ISendGridClient.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="SendGridClient.cs" company="SendGrid">
+﻿// <copyright file="ISendGridClient.cs" company="SendGrid">
 // Copyright (c) SendGrid. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
