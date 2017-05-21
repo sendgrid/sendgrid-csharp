58d35030bb9972d2b858ceb857f05e4db22d4a26
paritosh baghel <paritoshmmmec@gmail.com>
2017-05-21 08:02:28 -0400

    ExampleNet45ASPNetProject

---
+++ ExampleNet45ASPNetProject/SendGrid.ASPSamples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendGrid.ASPSamples.Startup))]
+namespace SendGrid.ASPSamples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
