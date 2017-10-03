70ce26dea3428e01eb877a8e8e0f7b23439fd18f
Kevek <kevin.r.kerr@gmail.com>
2017-10-02 22:04:26 -0400

    Fix #425: Implement IEquatable for EmailAddress

--- src/SendGrid/Helpers/Mail/Model/EmailAddress.cs
+++ src/SendGrid/Helpers/Mail/Model/EmailAddress.cs
@@ -3,6 +3,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System;
+
+
 namespace SendGrid.Helpers.Mail
 {
     using Newtonsoft.Json;
@@ -11,7 +14,7 @@ namespace SendGrid.Helpers.Mail
     /// An email object containing the email address and name of the sender or recipient.
     /// </summary>
     [JsonObject(IsReference = false)]
-    public class EmailAddress
+    public class EmailAddress : IEquatable<EmailAddress>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="EmailAddress"/> class.
@@ -42,5 +45,23 @@ namespace SendGrid.Helpers.Mail
         /// </summary>
         [JsonProperty(PropertyName = "email")]
         public string Email { get; set; }
+
+        public bool Equals(EmailAddress other) {
+            if(ReferenceEquals(null, other)) return false;
+            if(ReferenceEquals(this, other)) return true;
+            if(Email==null || other.Email==null) return false;
+            return string.Equals(Email.ToLowerInvariant(), other.Email.ToLowerInvariant());
+        }
+
+        public override bool Equals(object obj) {
+            if(ReferenceEquals(null, obj)) return false;
+            if(ReferenceEquals(this, obj)) return true;
+            if(obj.GetType()!=GetType()) return false;
+            return Equals((EmailAddress)obj);
+        }
+
+        public override int GetHashCode() {
+            return (Email!=null ? Email.ToLowerInvariant().GetHashCode() : 0);
+        }
     }
 }
