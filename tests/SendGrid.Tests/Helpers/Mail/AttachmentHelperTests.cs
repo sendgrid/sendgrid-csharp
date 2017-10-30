c0c1b6b7e32d3d83ecfdf938d50d6b6e3113b578
Adlan Razalan <adlan@notalan.co>
2017-10-30 23:50:08 +0800

    Add AttachmentHelper test

---
+++ tests/SendGrid.Tests/Helpers/Mail/AttachmentHelperTests.cs
@@ -0,0 +1,36 @@
+﻿namespace SendGrid.Tests.Mail
+{
+    using System.Threading.Tasks;
+    using SendGrid.Helpers.Mail;
+    using Xunit;
+    using System.Text;
+
+    public class AttachmentHelperTests
+    {
+        [Fact]
+        public async void AddAttachmentThroughHelper()
+        {
+            var attachmentSource = new FakeAttachmentSource("testfile.txt", "text/plain", Encoding.ASCII.GetBytes("Hello"));
+            var attachment = await AttachmentHelper.CreateAttachmentAsync(attachmentSource);
+
+            Assert.True(attachment.Filename == "testfile.txt");
+            Assert.True(attachment.Type == "text/plain");
+            Assert.True(attachment.Content == "SGVsbG8=");
+        }
+    }
+
+    public class FakeAttachmentSource : IAttachmentSource
+    {
+        private readonly AttachmentSource result;
+
+        public FakeAttachmentSource(string fileName, string mimeType, byte[] data)
+        {
+            result = new AttachmentSource(fileName, mimeType, data);
+        }
+
+        public Task<AttachmentSource> GetAttachmentAsync()
+        {
+            return Task.FromResult(result);
+        }
+    }
+}
