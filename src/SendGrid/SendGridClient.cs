06ffb7e6131653e2ec63078b4c1580b3ab4a9e7e
siddharth.kochar <siddharth.kochar@energage.com>
2018-06-16 15:54:07 +0530

    Fix warnings 1201, 1202

--- src/SendGrid/SendGridClient.cs
+++ src/SendGrid/SendGridClient.cs
@@ -26,21 +26,6 @@ namespace SendGrid
         private readonly SendGridClientOptions options = new SendGridClientOptions();
 
         /// <summary>
-        /// Gets or sets the path to the API resource.
-        /// </summary>
-        public string UrlPath { get; set; }
-
-        /// <summary>
-        /// Gets or sets the API version.
-        /// </summary>
-        public string Version { get; set; }
-
-        /// <summary>
-        /// Gets or sets the request media type.
-        /// </summary>
-        public string MediaType { get; set; }
-
-        /// <summary>
         /// The HttpClient instance to use for all calls from this SendGridClient instance.
         /// </summary>
         private HttpClient client;
@@ -93,25 +78,6 @@ namespace SendGrid
         /// Initializes a new instance of the <see cref="SendGridClient"/> class.
         /// </summary>
         /// <param name="httpClient">An optional http client which may me injected in order to facilitate testing.</param>
-        /// <param name="options">A <see cref="SendGridClientOptions"/> instance that defines the configuration settings to use with the client </param>
-        /// <returns>Interface to the SendGrid REST API</returns>
-        internal SendGridClient(HttpClient httpClient, SendGridClientOptions options)
-        {
-            if (options == null)
-            {
-                throw new ArgumentNullException(nameof(options));
-            }
-
-            this.options = options;
-            client = (httpClient == null) ? CreateHttpClientWithRetryHandler() : httpClient;
-
-            InitiateClient(options.ApiKey, options.Host, options.RequestHeaders, options.Version, options.UrlPath);
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SendGridClient"/> class.
-        /// </summary>
-        /// <param name="httpClient">An optional http client which may me injected in order to facilitate testing.</param>
         /// <param name="apiKey">Your SendGrid API key.</param>
         /// <param name="host">Base url (e.g. https://api.sendgrid.com)</param>
         /// <param name="requestHeaders">A dictionary of request headers</param>
@@ -138,63 +104,22 @@ namespace SendGrid
         }
 
         /// <summary>
-        /// Common method to initiate internal fields regardless of which constructor was used.
+        /// Initializes a new instance of the <see cref="SendGridClient"/> class.
         /// </summary>
-        /// <param name="apiKey">Your SendGrid API key.</param>
-        /// <param name="host">Base url (e.g. https://api.sendgrid.com)</param>
-        /// <param name="requestHeaders">A dictionary of request headers</param>
-        /// <param name="version">API version, override AddVersion to customize</param>
-        /// <param name="urlPath">Path to endpoint (e.g. /path/to/endpoint)</param>
-        private void InitiateClient(string apiKey, string host, Dictionary<string, string> requestHeaders, string version, string urlPath)
+        /// <param name="httpClient">An optional http client which may me injected in order to facilitate testing.</param>
+        /// <param name="options">A <see cref="SendGridClientOptions"/> instance that defines the configuration settings to use with the client </param>
+        /// <returns>Interface to the SendGrid REST API</returns>
+        internal SendGridClient(HttpClient httpClient, SendGridClientOptions options)
         {
-            UrlPath = urlPath;
-            Version = version;
-
-            var baseAddress = host ?? "https://api.sendgrid.com";
-            var clientVersion = GetType().GetTypeInfo().Assembly.GetName().Version.ToString();
-
-            // standard headers
-            client.BaseAddress = new Uri(baseAddress);
-            Dictionary<string, string> headers = new Dictionary<string, string>
+            if (options == null)
             {
-                { "Authorization", "Bearer " + apiKey },
-                { "Content-Type", "application/json" },
-                { "User-Agent", "sendgrid/" + clientVersion + " csharp" },
-                { "Accept", "application/json" }
-            };
-
-            // set header overrides
-            if (requestHeaders != null)
-            {
-                foreach (var header in requestHeaders)
-                {
-                    headers[header.Key] = header.Value;
-                }
+                throw new ArgumentNullException(nameof(options));
             }
 
-            // add headers to httpClient
-            foreach (var header in headers)
-            {
-                if (header.Key == "Authorization")
-                {
-                    var split = header.Value.Split();
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(split[0], split[1]);
-                }
-                else if (header.Key == "Content-Type")
-                {
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(header.Value));
-                    MediaType = header.Value;
-                }
-                else
-                {
-                    client.DefaultRequestHeaders.Add(header.Key, header.Value);
-                }
-            }
-        }
+            this.options = options;
+            client = (httpClient == null) ? CreateHttpClientWithRetryHandler() : httpClient;
 
-        private HttpClient CreateHttpClientWithRetryHandler()
-        {
-            return new HttpClient(new RetryDelegatingHandler(options.ReliabilitySettings));
+            InitiateClient(options.ApiKey, options.Host, options.RequestHeaders, options.Version, options.UrlPath);
         }
 
         /// <summary>
@@ -227,6 +152,21 @@ namespace SendGrid
             /// </summary>
             PUT
         }
+
+        /// <summary>
+        /// Gets or sets the path to the API resource.
+        /// </summary>
+        public string UrlPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the API version.
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the request media type.
+        /// </summary>
+        public string MediaType { get; set; }
 
         /// <summary>
         /// Add the authorization header, override to customize
@@ -303,6 +243,66 @@ namespace SendGrid
                 msg.Serialize(),
                 urlPath: "mail/send",
                 cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Common method to initiate internal fields regardless of which constructor was used.
+        /// </summary>
+        /// <param name="apiKey">Your SendGrid API key.</param>
+        /// <param name="host">Base url (e.g. https://api.sendgrid.com)</param>
+        /// <param name="requestHeaders">A dictionary of request headers</param>
+        /// <param name="version">API version, override AddVersion to customize</param>
+        /// <param name="urlPath">Path to endpoint (e.g. /path/to/endpoint)</param>
+        private void InitiateClient(string apiKey, string host, Dictionary<string, string> requestHeaders, string version, string urlPath)
+        {
+            UrlPath = urlPath;
+            Version = version;
+
+            var baseAddress = host ?? "https://api.sendgrid.com";
+            var clientVersion = GetType().GetTypeInfo().Assembly.GetName().Version.ToString();
+
+            // standard headers
+            client.BaseAddress = new Uri(baseAddress);
+            Dictionary<string, string> headers = new Dictionary<string, string>
+            {
+                { "Authorization", "Bearer " + apiKey },
+                { "Content-Type", "application/json" },
+                { "User-Agent", "sendgrid/" + clientVersion + " csharp" },
+                { "Accept", "application/json" }
+            };
+
+            // set header overrides
+            if (requestHeaders != null)
+            {
+                foreach (var header in requestHeaders)
+                {
+                    headers[header.Key] = header.Value;
+                }
+            }
+
+            // add headers to httpClient
+            foreach (var header in headers)
+            {
+                if (header.Key == "Authorization")
+                {
+                    var split = header.Value.Split();
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(split[0], split[1]);
+                }
+                else if (header.Key == "Content-Type")
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(header.Value));
+                    MediaType = header.Value;
+                }
+                else
+                {
+                    client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+            }
+        }
+
+        private HttpClient CreateHttpClientWithRetryHandler()
+        {
+            return new HttpClient(new RetryDelegatingHandler(options.ReliabilitySettings));
         }
 
         /// <summary>
