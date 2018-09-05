dc345b34af287b41cc4bebf8a5522fccb0af290d
Elmer Thomas <elmer@thinkingserious.com>
2018-09-05 12:20:44 -0700

    Naming conventions

---
+++ tests/SendGrid.Tests/TemplateDataSerialisationTests.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using SendGrid.Helpers.Mail;
+using Xunit;
+
+namespace SendGrid.Tests
+{
+    public class TemplateDataSerialisationTests
+    {
+        [Fact]
+        public void TestSetTemplateDataWorksWithSpecifiedJsonPropertyNames()
+        {
+            var msg = new SendGridMessage();
+
+            var dynamicTemplateData = new TestTemplateData
+            {
+                MyCamelCaseProperty = "camelCase",
+                MyKebabCaseProperty = "kebab-case",
+                MyPascalCaseProperty = "PascalCase",
+                MySnakeCaseProperty = "snake_case",
+            };
+
+            msg.SetTemplateData(dynamicTemplateData);
+            Assert.Equal("{\"personalizations\":[{\"dynamic_template_data\":{\"myCamelCaseProperty\":\"camelCase\",\"my-kebab-case-property\":\"kebab-case\",\"MyPascalCaseProperty\":\"PascalCase\",\"my_snake_case_property\":\"snake_case\"}}]}", msg.Serialize());
+        }
+
+        private class TestTemplateData
+        {
+            [JsonProperty("myCamelCaseProperty")]
+            public string MyCamelCaseProperty { get; set; }
+
+            [JsonProperty("my-kebab-case-property")]
+            public string MyKebabCaseProperty { get; set; }
+
+            public string MyPascalCaseProperty { get; set; }
+
+            [JsonProperty("my_snake_case_property")]
+            public string MySnakeCaseProperty { get; set; }
+        }
+    }
+}
