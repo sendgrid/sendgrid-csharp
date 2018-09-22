857129becafb67297d1c66d305c90cc40c76f64e
Darren Maddox <da1rren@gmail.com>
2018-09-22 14:25:46 +0100

    Began implementation of #266
    * Added the extension method to sendgrid lib
    * Added some basic tests
    * Raised test project to .net core 2.1 (Need so I have the types to test with)

---
+++ src/SendGrid/Helpers/Mail/MailMessageExtensions.cs
@@ -0,0 +1,109 @@
+﻿// <copyright file="MailMessageExtensions.cs" company="SendGrid">
+// Copyright (c) SendGrid. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+#if NETSTANDARD2_0 || NET452
+
+namespace SendGrid.Helpers.Mail
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Mail;
+
+    public static class MailMessageExtensions
+    {
+        public static EmailAddress GetSendGridAddress(this MailAddress address)
+        {
+            return string.IsNullOrWhiteSpace(address.DisplayName) ?
+                new EmailAddress(address.Address) :
+                new EmailAddress(address.Address, address.DisplayName.Replace(",", "").Replace(";", ""));
+        }
+
+        public static SendGrid.Helpers.Mail.Attachment GetSendGridAttachment(this System.Net.Mail.Attachment attachment)
+        {
+            using (var stream = new MemoryStream())
+            {
+                attachment.ContentStream.CopyTo(stream);
+                return new SendGrid.Helpers.Mail.Attachment()
+                {
+                    Disposition = "attachment",
+                    Type = attachment.ContentType.MediaType,
+                    Filename = attachment.Name,
+                    ContentId = attachment.ContentId,
+                    Content = Convert.ToBase64String(stream.ToArray())
+                };
+            }
+        }
+
+        public static SendGridMessage GetSendGridMessage(this MailMessage message)
+        {
+            var sendgridMessage = new SendGridMessage();
+
+            sendgridMessage.From = GetSendGridAddress(message.From);
+
+            if (message.ReplyToList.Any())
+            {
+                sendgridMessage.ReplyTo = message.ReplyToList.First().GetSendGridAddress();
+            }
+
+            if (message.To.Any())
+            {
+                var tos = message.To.Select(x => x.GetSendGridAddress()).ToList();
+                sendgridMessage.AddTos(tos);
+            }
+
+            if (message.CC.Any())
+            {
+                var cc = message.CC.Select(x => x.GetSendGridAddress()).ToList();
+                sendgridMessage.AddCcs(cc);
+            }
+
+            if (message.Bcc.Any())
+            {
+                var bcc = message.Bcc.Select(x => x.GetSendGridAddress()).ToList();
+                sendgridMessage.AddBccs(bcc);
+            }
+
+            if (!string.IsNullOrWhiteSpace(message.Subject))
+            {
+                sendgridMessage.Subject = message.Subject;
+            }
+
+            if (!string.IsNullOrWhiteSpace(message.Body))
+            {
+                var content = message.Body;
+
+                if (message.IsBodyHtml)
+                {
+
+                    if (content.StartsWith("<html"))
+                    {
+                        content = message.Body;
+                    }
+                    else
+                    {
+                        content = $"<html><body>{message.Body}</body></html>";
+                    }
+
+                    sendgridMessage.AddContent("text/html", content);
+                }
+                else
+                {
+                    sendgridMessage.AddContent("text/plain", content);
+                }
+            }
+
+            if (message.Attachments.Any())
+            {
+                sendgridMessage.Attachments = new System.Collections.Generic.List<SendGrid.Helpers.Mail.Attachment>();
+                sendgridMessage.Attachments.AddRange(message.Attachments.Select(x => GetSendGridAttachment(x)));
+            }
+
+            return sendgridMessage;
+        }
+
+    }
+}
+#endif
---
+++ tests/SendGrid.Tests/Helpers/Mail/MailMessageExtensionsTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Net.Mail;
+using SendGrid.Helpers.Mail;
+using Xunit;
+
+namespace SendGrid.Tests.Helpers.Mail
+{
+    public class MailMessageExtensionsTests
+    {
+        [Theory]
+        [InlineData("Example@example.com")]
+        [InlineData("Example+data@example.com")]
+        public void ConvertEmailAddresses(string email)
+        {
+            var address = new MailAddress(email).GetSendGridAddress();
+            Assert.Equal(email, address.Email);
+        }
+
+        [Theory]
+        [InlineData("Example@example.com", "Ben@example.com", "Widget Factory Production", "Ben, widget production is down 15%, could you investigate?")]
+        public void TestSimpleMailMessageEmail(string from, string to, string subject, string body)
+        {
+            var mail = new MailMessage(from, to)
+            {
+                Subject = subject,
+
+            };
+
+            mail.Body = body;
+
+            var message = mail.GetSendGridMessage();
+
+            Assert.Equal(from, message.From.Email);
+            Assert.Equal(subject, message.Subject);
+            Assert.Contains(message.Personalizations.SelectMany(x => x.Tos), x => x.Email == to);
+            Assert.Contains(message.Contents, x => x.Value == body);
+        }
+
+        [Theory]
+        [InlineData("Example@example.com", "Ben@example.com", "Widget Factory Production", "Management@example.com", "HR@example.com")]
+        public void TestMailMessageEmailWithCC(string from, string to, string subject, string cc, string bcc)
+        {
+            var mail = new MailMessage(from, to)
+            {
+                Subject = subject
+            };
+
+            mail.CC.Add(cc);
+            mail.Bcc.Add(bcc);
+
+            var message = mail.GetSendGridMessage();
+
+
+            Assert.Equal(from, message.From.Email);
+            Assert.Equal(subject, message.Subject);
+            Assert.Contains(message.Personalizations.SelectMany(x => x.Tos), x => x.Email == to);
+            Assert.Contains(message.Personalizations.SelectMany(x => x.Ccs), x => x.Email == cc);
+            Assert.Contains(message.Personalizations.SelectMany(x => x.Bccs), x => x.Email == bcc);
+        }
+
+        [Theory]
+        [InlineData("Example@example.com", "Ben@example.com", "invoicing@example.com")]
+        public void TestSimpleMailMessageReplyTo(string from, string to, string replyTo)
+        {
+            var mail = new MailMessage(from, to);
+            mail.ReplyToList.Add(replyTo);
+
+            var message = mail.GetSendGridMessage();
+
+            Assert.Equal(replyTo, message.ReplyTo.Email);
+        }
+    }
+}
