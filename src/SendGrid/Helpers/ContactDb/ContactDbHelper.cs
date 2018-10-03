2fe19a492564bcf63095151e9789c99e32045c88
Xavier Hahn <xavier.hahn@gmail.com>
2018-10-03 22:22:00 +0200

    Creates ContactDbHelper class
    Creates the new folder that contains the contact db helper,
    new namespace and new contact helper class.
    
    Start working on issue #750

---
+++ src/SendGrid/Helpers/ContactDb/ContactDbHelper.cs
@@ -0,0 +1,15 @@
+// <copyright file="ContactDbHelper.cs" company="SendGrid">
+// Copyright (c) SendGrid. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace SendGrid.Helpers.ContactDb
+{
+    /// <summary>
+    /// Simplified contact database management for common use cases
+    /// </summary>
+    public class ContactHelper
+    {
+        
+    }
+}
