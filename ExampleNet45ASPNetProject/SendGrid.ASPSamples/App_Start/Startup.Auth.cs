1968d20f7e8b6a3c256321ad63263362d24eb453
Hestersue43 <40472322+Hestersue43@users.noreply.github.com>
2018-10-30 22:31:55 -0400

    Update Startup.Auth.cs

--- ExampleNet45ASPNetProject/SendGrid.ASPSamples/App_Start/Startup.Auth.cs
+++ ExampleNet45ASPNetProject/SendGrid.ASPSamples/App_Start/Startup.Auth.cs
@@ -14,14 +14,14 @@ namespace SendGrid.ASPSamples
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
+            // Configure the db context, user manager and sign-in manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
-            // Enable the application to use a cookie to store information for the signed in user
+            // Enable the application to use a cookie to store information for the signed-in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
+            // Configure the sign-in cookie
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
