56da7a9232c334a3e5cc5459279466bb69d27850
Jonas Nyrup <jnyrup@users.noreply.github.com>
2019-06-28 09:41:44 +0200

    Replace nuspec with csproj (#892)

--- src/SendGrid/Properties/AssemblyInfo.cs
+++ src/SendGrid/Properties/AssemblyInfo.cs
@@ -3,16 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCompany("Twilio SendGrid")]
-[assembly: AssemblyProduct("Twilio SendGrid")]
-[assembly: AssemblyTrademark("Twilio SendGrid")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,5 +12,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("377c20e4-2297-488f-933b-fb635c56d8fc")]
-
-[assembly: AssemblyInformationalVersion("9.11.0")]
