4c24eef0059d4de5734a9afa76c4860e77dd87b2
aevitas <m4rtijn@gmail.com>
2019-07-04 16:56:22 +0200

    Add EmailAddress equality tests

---
+++ tests/SendGrid.Tests/Helpers/Mail/EmailAddressTests.cs
@@ -0,0 +1,22 @@
+﻿using SendGrid.Helpers.Mail;
+using Xunit;
+
+namespace SendGrid.Tests.Helpers.Mail
+{
+    public class EmailAddressTests
+    {
+        [Fact]
+        public void TestEmailAddressEquality()
+        {
+            var left = new EmailAddress("test@sendgrid.com", "Test");
+            var right = new EmailAddress("test@sendGrid.com", "Test");
+            var up = new EmailAddress("test@sendgrid.com", "test");
+            var down = new EmailAddress("test@sendgrid.com", "Test");
+
+            Assert.True(left.Equals(left));
+            Assert.True(left.Equals(down));
+            Assert.False(left.Equals(right));
+            Assert.False(left.Equals(up));
+        }
+    }
+}
