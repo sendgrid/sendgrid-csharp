236a4a2063058dbe9f305a7ca7934b31458d1dd1
aevitas <m4rtijn@gmail.com>
2019-07-04 16:46:54 +0200

    Implement IEquatable in EmailAddress

--- src/SendGrid/Helpers/Mail/Model/EmailAddress.cs
+++ src/SendGrid/Helpers/Mail/Model/EmailAddress.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System;
 using Newtonsoft.Json;
 
 namespace SendGrid.Helpers.Mail
@@ -11,7 +12,7 @@ namespace SendGrid.Helpers.Mail
     /// An email object containing the email address and name of the sender or recipient.
     /// </summary>
     [JsonObject(IsReference = false)]
-    public class EmailAddress
+    public class EmailAddress : IEquatable<EmailAddress>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="EmailAddress"/> class.
@@ -42,5 +43,42 @@ namespace SendGrid.Helpers.Mail
         /// </summary>
         [JsonProperty(PropertyName = "email")]
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="EmailAddress"/> is equal to the specified EmailAddress.
+        /// </summary>
+        /// <param name="other">The comparand email address.</param>
+        /// <returns>true if the objects are equal, false if they're not.</returns>
+        public bool Equals(EmailAddress other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Name, other.Name) && string.Equals(Email, other.Email);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="EmailAddress"/> is equal to the specified object.
+        /// </summary>
+        /// <param name="obj">The comparand object.</param>
+        /// <returns>true if the objects are equal, false if they're not.</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((EmailAddress)obj);
+        }
+
+        /// <summary>
+        /// Gets a hash code representing this object.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Name != null ? Name.GetHashCode() : 0) * 397) ^ (Email != null ? Email.GetHashCode() : 0);
+            }
+        }
     }
 }
