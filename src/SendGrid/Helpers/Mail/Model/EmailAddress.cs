34e42bd8c08d71560a598e47ff4806ddebeb42c3
aevitas <m4rtijn@gmail.com>
2019-07-14 20:38:00 +0200

    Implement equality operators and follow coding conventions closer

--- src/SendGrid/Helpers/Mail/Model/EmailAddress.cs
+++ src/SendGrid/Helpers/Mail/Model/EmailAddress.cs
@@ -3,8 +3,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using System;
 using Newtonsoft.Json;
+using System;
 
 namespace SendGrid.Helpers.Mail
 {
@@ -45,15 +45,50 @@ namespace SendGrid.Helpers.Mail
         public string Email { get; set; }
 
         /// <summary>
+        /// Determines whether the two specified operands are equal.
+        /// </summary>
+        /// <param name="left">The left hand operand in the equation.</param>
+        /// <param name="right">The right hand operand in the equation.</param>
+        /// <returns>True if equal, false if not.</returns>
+        public static bool operator ==(EmailAddress left, EmailAddress right)
+        {
+            if (left == null && right == null)
+            {
+                return true;
+            }
+
+            return left?.Equals(right) ?? false;
+        }
+
+        /// <summary>
+        /// Determines whether the two specified operands are not equal.
+        /// </summary>
+        /// <param name="left">The left hand operand in the equation.</param>
+        /// <param name="right">The right hand operand in the equation.</param>
+        /// <returns>True if the two operands are not equal, and false if they are.</returns>
+        public static bool operator !=(EmailAddress left, EmailAddress right)
+        {
+            return !(left == right);
+        }
+
+        /// <summary>
         /// Gets a value indicating whether this <see cref="EmailAddress"/> is equal to the specified EmailAddress.
         /// </summary>
         /// <param name="other">The comparand email address.</param>
         /// <returns>true if the objects are equal, false if they're not.</returns>
         public bool Equals(EmailAddress other)
         {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return string.Equals(Name, other.Name) && string.Equals(Email, other.Email);
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return string.Equals(this.Name, other.Name) && string.Equals(this.Email, other.Email);
         }
 
         /// <summary>
@@ -63,21 +98,33 @@ namespace SendGrid.Helpers.Mail
         /// <returns>true if the objects are equal, false if they're not.</returns>
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((EmailAddress)obj);
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((EmailAddress) obj);
         }
 
         /// <summary>
         /// Gets a hash code representing this object.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A hash code representing this instance.</returns>
         public override int GetHashCode()
         {
             unchecked
             {
-                return ((Name != null ? Name.GetHashCode() : 0) * 397) ^ (Email != null ? Email.GetHashCode() : 0);
+                return ((this.Name != null ? this.Name.GetHashCode() : 0) * 397) ^ (this.Email != null ? this.Email.GetHashCode() : 0);
             }
         }
     }
