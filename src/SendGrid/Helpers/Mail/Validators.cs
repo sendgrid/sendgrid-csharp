d8b8faec3df3795c11b722a6f9ffb2f1d3bdaf42
Hariharan Subramanian <hariharan.subramanian@biztalk360.co.uk>
2019-09-03 13:59:05 +0530

    Added validations for CCs/ Bcc methods

---
+++ src/SendGrid/Helpers/Mail/Validators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SendGrid.Helpers.Mail
+{
+	public static class Validators
+	{
+		public static bool IsValid(this EmailAddress emailAddress)
+		{
+			return !string.IsNullOrEmpty(emailAddress.Email) && !string.IsNullOrEmpty(emailAddress.Name);
+		}
+
+		public static bool IsValid(this List<EmailAddress> addresses)
+		{
+			return addresses != null && !addresses.Any();
+		}
+	}
+}
