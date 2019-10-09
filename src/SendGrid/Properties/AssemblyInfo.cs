1720aa0251ad6c1ef7f021eccccd0cd4872fdcac
aevitas <m4rtijn@gmail.com>
2019-10-09 13:02:10 +0200

    Expose SendGridClient internals to test project

--- src/SendGrid/Properties/AssemblyInfo.cs
+++ src/SendGrid/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -12,3 +13,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("377c20e4-2297-488f-933b-fb635c56d8fc")]
+
+[assembly: InternalsVisibleTo("SendGrid.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100812ec26a66c8e08c790704ac4b46bcc9da9f4bca4da0ec7c06ce6dcd73baeb2c5525f36a237b253e80e16febb4c052f50734d5e1cf3bf478d9c88f0f69df53b47306419182983bc35c33c3bafb5e90b9bd7aa7b9a9da09abe3667d50db891012e077e4b9aefe9799a58222fa67127c230219755d7670073c7463d90cf9e79dba")]
