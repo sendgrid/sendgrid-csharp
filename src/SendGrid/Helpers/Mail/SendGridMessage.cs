df32babed5654427482e5751b3045e0214eb4e73
indy-singh <1906778+indy-singh@users.noreply.github.com>
2020-02-26 20:06:49 +0000

    Got resharped

--- src/SendGrid/Helpers/Mail/SendGridMessage.cs
+++ src/SendGrid/Helpers/Mail/SendGridMessage.cs
@@ -7,7 +7,6 @@ using Newtonsoft.Json;
 using SendGrid.Helpers.Mail.Model;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
