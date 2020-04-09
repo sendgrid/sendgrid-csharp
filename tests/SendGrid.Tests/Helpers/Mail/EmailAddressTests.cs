eeae9690c47e7a78c270241f1d9e776c7c27f19a
childish-sambino <sharrison@twilio.com>
2020-04-09 16:19:37 -0500

    Update EmailAddressTests.cs

--- tests/SendGrid.Tests/Helpers/Mail/EmailAddressTests.cs
+++ tests/SendGrid.Tests/Helpers/Mail/EmailAddressTests.cs
@@ -1,4 +1,4 @@
-﻿using SendGrid.Helpers.Mail;
+using SendGrid.Helpers.Mail;
 using Xunit;
 
 namespace SendGrid.Tests.Helpers.Mail
@@ -8,15 +8,15 @@ namespace SendGrid.Tests.Helpers.Mail
         [Fact]
         public void TestEmailAddressEquality()
         {
-            var left = new EmailAddress("test@sendgrid.com", "Test");
-            var right = new EmailAddress("test@sendGrid.com", "Test");
-            var up = new EmailAddress("test@sendgrid.com", "test");
-            var down = new EmailAddress("test@sendgrid.com", "Test");
+            var left = new EmailAddress("test1@sendgrid.com", "test");
+            var right = new EmailAddress("test1@sendGrid.com", "Test");
+            var up = new EmailAddress("test2@sendgrid.com", "test");
+            var down = new EmailAddress("test2@sendgrid.com", "Test");
 
             Assert.True(left.Equals(left));
-            Assert.True(left.Equals(down));
-            Assert.False(left.Equals(right));
+            Assert.True(left.Equals(right));
             Assert.False(left.Equals(up));
+            Assert.False(left.Equals(down));
         }
     }
 }
