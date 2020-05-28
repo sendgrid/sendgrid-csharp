cadb619143e7279b4af2c8c5dbc2a3c6e2d1ea28
Charley Wu <akunzai@gmail.com>
2020-05-29 04:03:17 +0800

    Integrate SendGrid with HttpClientFactory (#922)
    * Integrate SendGrid with HttpClientFactory
    
    - Introduce new SendGrid.Extensions.DependencyInjection package for HttpClientFactory integrations
    
    * Update SendGrid.Tests
    
    - Update NuGet packages
    - Cleanup Properties
    
    * Add Solution Items
    
    * Renamed `Example.cs` to `Program.cs`
    
    - `Program.cs` is the convention of C# console application entry point
    
    * Build .NET Framework libraries on Linux without Mono
    
    - https://andrewlock.net/using-reference-assemblies-to-build-net-framework-libararies-on-linux-without-mono/
    
    * Simplify Examples
    
    - Upgrade to SDK style csproj
    - Code cleanup
    
    * Simplify Makefile
    
    * Update README.md
    
    - Add `SendGrid.Extensions.DependencyInjection` usage
    - Fixes typo
    - Fixes Markdown syntax warnings
    - Simplify examples
    
    * Use .NET Core CLI for *.nupkg deployment
    
    - https://docs.microsoft.com/dotnet/core/tools/dotnet-nuget-push

---
+++ ExampleCoreProject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SendGrid;
+using SendGrid.Extensions.DependencyInjection;
+using SendGrid.Helpers.Mail;
+
+namespace Example
+{
+    internal class Program
+    {
+        private static IConfiguration Configuration { get; set; }
+
+        private static async Task Main()
+        {
+            var env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
+            Configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: true)
+                    .AddJsonFile($"appsettings.{env}.json", optional: true)
+                    .Build();
+            var services = ConfigureServices(new ServiceCollection()).BuildServiceProvider();
+            var client = services.GetRequiredService<ISendGridClient>();
+            var from = new EmailAddress(Configuration.GetValue("SendGrid:From", "test@example.com"), "Example User");
+            var to = new EmailAddress(Configuration.GetValue("SendGrid:To", "test@example.com"), "Example User");
+            var msg = new SendGridMessage
+            {
+                From = from,
+                Subject = "Sending with Twilio SendGrid is Fun"
+            };
+            msg.AddContent(MimeType.Text, "and easy to do anywhere, even with C#");
+            msg.AddTo(to);
+            if (Configuration.GetValue("SendGrid:SandboxMode", false))
+            {
+                msg.MailSettings = new MailSettings
+                {
+                    SandboxMode = new SandboxMode
+                    {
+                        Enable = true
+                    }
+                };
+            }
+            Console.WriteLine($"Sending email with payload: \n{msg.Serialize()}");
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+
+            Console.WriteLine($"Response: {response.StatusCode}");
+            Console.WriteLine(response.Headers);
+        }
+
+        private static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddSendGrid(options => { options.ApiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY") ?? Configuration["SendGrid:ApiKey"]; });
+
+            return services;
+        }
+    }
+}
