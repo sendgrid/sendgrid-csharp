bfb6bff784086064ed22c81030fe56222247c5ec
Sam Harrison <sharrison@twilio.com>
2020-06-17 14:54:28 -0500

    feat: add Elliptic Curve code and utilities
    See README for details.

---
+++ src/SendGrid/EllipticCurve/utils/string.cs
@@ -0,0 +1,22 @@
+﻿namespace EllipticCurve.Utils
+{
+
+    public static class String
+    {
+
+        public static string substring(string str, int index, int length)
+        {
+            if (str.Length > index + length)
+            {
+                return str.Substring(index, length);
+            }
+            if (str.Length > index)
+            {
+                return str.Substring(index);
+            }
+            return "";
+        }
+
+    }
+
+}
