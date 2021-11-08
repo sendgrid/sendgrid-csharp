212c8d8c1c805935e42e53ef0f69b8c408b72523
Bilal Boussayoud <bboussayoud@twilio.com>
2021-11-08 09:25:02 -0700

    Revert "Delete Program.cs"
    This reverts commit ed8f615843edd652a951accb85a9b0049e94c900.

---
+++ ExampleCoreProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Example
+{
+    internal class ExampleEmail
+    {
+        private static void Main()
+        {
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("team_intrapersonalfaces@devinterfaces.com");
+            var subject = "Hello from Twilio SendGrid";
+
+            var msg = new SendGridMessage();
+            msg.Subject = subject;
+            msg.AddContent(MimeType.Text, "Hello from fakegrid, please work");
+            msg.SetFrom(from);
+
+            msg.Personalizations = new List<Personalization>() {
+                new Personalization() {
+                    Tos = new List<EmailAddress>() {
+                        new EmailAddress("bboussayoud@colgate.edu")
+                    },
+                },
+                new Personalization() {
+                    Tos = new List<EmailAddress>() {
+                        new EmailAddress("bilal.boussayoud@gmail.com")
+                    },
+                    From = new EmailAddress("team_nofaces@devinterfaces.com")
+                },
+            };
+            var response = await client.SendEmailAsync(msg);
+
+            Console.WriteLine(msg.Serialize());
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Headers.ToString());
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
